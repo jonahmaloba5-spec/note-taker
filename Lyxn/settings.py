0780d1ff0727871c972153a61b8ebf782d6f256e
jonahmaloba5-spec <jonahmaloba5@gmail.com>
2026-01-30 20:08:19 +0300

    update settings

--- Lyxn/settings.py
+++ Lyxn/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = "Lyxn.urls"
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
